431326b7b4e48fbaa6d2bae2a850b4ed44cb18cd
petterhj <post@petterhj.net>
2019-12-11 21:38:10 +0100

    queue event on client connected

--- socket_server.py
+++ socket_server.py
@@ -1,20 +1,44 @@
 # Imports
-import socketio
-
-from logger import logger
-
-
-# Server
-sio = socketio.AsyncServer(async_mode='aiohttp')
+from socketio import AsyncServer
+from loguru import logger
 
 
-# Event
-@sio.event
-async def event(sid, data):
-    # print('-'*50)
-    # print(sid)
-    # print(data)
-    # print('-'*50)
-    sio.context.queue_event(data['source'], data['name'])
-    
-    # await sio.emit('message', {'response': 'my response'})
+
+# Class: Socket server
+class SocketServer(AsyncServer):
+    # Configure
+    def configure(self, context):
+        logger.info('Configuring socket server')
+
+        self.context = context
+
+        # Attach the socket server to the application
+        self.attach(self.context.http_server)
+
+        # Register event handlers
+        self.on('connect', self.on_connect)
+        self.on('event', self.on_event)
+
+
+    # Event: Connect
+    async def on_connect(self, sid, environ):
+        logger.info('Client connected, sid=%s' % (sid))
+
+        self.context.queue_event('homeghost', 'client.connected', payload={
+            'sid': sid
+        })
+
+        await self.emit('status', self.context.status, room=sid)
+
+
+    # Event: Event
+    async def on_event(self, sid, data):
+        source = data.get('source')
+        name = data.get('name')
+
+        if not source or not name and 'event' in data:
+            event = data['event'].split('.')
+            source = event[0]
+            name = '.'.join(event[1:])
+
+        self.context.queue_event(source, name)
